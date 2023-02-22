31da948f2e794ade643c5684da86af06d02f6b97
Chanaka Subasingha <chanaka.s@icloud.com>
2023-02-22 16:11:13 +0100

    added security feature

--- backend/src/main/java/com/example/demo/config/SecurityConfiguration.java
+++ backend/src/main/java/com/example/demo/config/SecurityConfiguration.java
@@ -1,52 +1,51 @@
-// package com.example.demo.config;
+package com.example.demo.config;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.http.HttpMethod;
-// import
-// org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-// import
-// org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import
-// org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-// @Configuration
-// @EnableWebSecurity
-// public class SecurityConfiguration {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
 
-// @Bean
-// public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws
-// Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 
-// httpSecurity.httpBasic();
-// httpSecurity.csrf().disable();
+        httpSecurity.httpBasic();
+        httpSecurity.csrf().disable();
 
-// httpSecurity
-// .authorizeHttpRequests()
-// .requestMatchers(HttpMethod.POST, "/users/**")
-// .permitAll()
-// .and()
-// .authorizeHttpRequests()
-// .requestMatchers(HttpMethod.GET, "/products/**")
-// .permitAll()
-// .and()
-// .authorizeHttpRequests()
-// .requestMatchers(HttpMethod.POST, "/products").hasRole("ADMIN")
-// .requestMatchers(HttpMethod.PUT, "/products/**").hasRole("ADMIN")
-// .requestMatchers(HttpMethod.DELETE, "/products/**").hasRole("ADMIN")
-// .requestMatchers(HttpMethod.POST, "/orders").hasRole("CUSTOMER");
+        httpSecurity
+                .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.POST, "/users/**")
+                .permitAll()
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.GET, "/products/**")
+                .permitAll()
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.POST, "/products").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/products/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/products/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/orders").hasRole("CUSTOMER")
+                .requestMatchers(HttpMethod.GET, "/orders").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/orders/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/orders/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/orders/**").hasRole("ADMIN");
 
-// return httpSecurity.build();
-// }
+        return httpSecurity.build();
+    }
 
-// @Autowired
-// public void configureGlobal(AuthenticationManagerBuilder auth) throws
-// Exception {
-// auth.inMemoryAuthentication()
-// .withUser("admin").password("{noop}admin").roles("ADMIN")
-// .and()
-// .withUser("customer").password("{noop}customer").roles("ADMIN", "CUSTOMER");
-// }
-// }
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("{noop}admin").roles("ADMIN")
+                .and()
+                .withUser("customer").password("{noop}customer").roles("ADMIN", "CUSTOMER");
+    }
+}
--- backend/src/main/java/com/example/demo/repository/ProductRepository.java
+++ backend/src/main/java/com/example/demo/repository/ProductRepository.java
@@ -5,18 +5,7 @@ import com.example.demo.models.Product;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-// import org.springframework.security.access.prepost.PreAuthorize;
 
-// @PreAuthorize("hasRole('ROLE_USER')")
 public interface ProductRepository extends MongoRepository<Product, String> {
-
-    // @PreAuthorize("hasRole('ROLE_ADMIN')")
-    // @Override
-    // <S extends Product> S save(S entity);
-
-    // @PreAuthorize("hasRole('ROLE_ADMIN')")
-    // @Override
-    // void delete(Product product);
-
     List<Product> findByCategory(String category);
 }
