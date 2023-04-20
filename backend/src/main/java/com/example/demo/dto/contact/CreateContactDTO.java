1e5e44668a99d0437e2a75b308fa29f77ca80934
Chanaka Subasinghage <chanaka.s@icloud.com>
2023-04-20 13:08:02 +0200

    done

---
+++ backend/src/main/java/com/example/demo/dto/contact/CreateContactDTO.java
@@ -0,0 +1,38 @@
+package com.example.demo.dto.user;
+
+import jakarta.validation.constraints.NotNull;
+
+public class CreateContactDTO {
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String password;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String email) {
+        this.username = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
