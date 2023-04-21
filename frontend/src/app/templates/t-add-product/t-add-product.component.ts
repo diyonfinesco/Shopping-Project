e5c34b2a053df67e5461070bdaaa12275e3eb268
Chanaka Subasinghage <chanaka.s@icloud.com>
2023-04-21 09:21:47 +0200

    modified

--- frontend/src/app/templates/t-add-product/t-add-product.component.ts
+++ frontend/src/app/templates/t-add-product/t-add-product.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ProductService } from 'src/app/services/product.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-t-add-product',
@@ -8,7 +10,7 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: []
 })
 export class TAddProductComponent {
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private router: Router, private userService: UserService) { }
   @Output() onCreateProduct: EventEmitter<void> = new EventEmitter();
 
   productForm = new FormGroup({
@@ -19,6 +21,12 @@ export class TAddProductComponent {
     price: new FormControl(0, [Validators.required, Validators.min(1)]),
     quantity: new FormControl(0, [Validators.required, Validators.min(1)])
   })
+
+  ngOnInit(): void {
+    if (!this.userService.isAdmin()) {
+      this.router.navigateByUrl('/')
+    }
+  }
 
   toFormData(formValue: any) {
     const formData = new FormData();
